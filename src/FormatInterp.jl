cea4b1b4ac217bc99e81ba651b6fd42a2859d362
Alexander Plavin <alexander@plav.in>
2021-01-01 00:00:00 +0000

    refactor and improve parsing, add tests

--- src/FormatInterp.jl
+++ src/FormatInterp.jl
@@ -6,94 +6,89 @@ using Formatting
 
 Formatting._srepr(x::Symbol) = string(x)
 
+abstract type State end
+struct Plain <: State end
+struct SeenLbrace <: State end
+struct SeenRbrace <: State end
+struct InBraces <: State
+    nest_level::Int
+    content::String
+end
+
+abstract type Token end
+struct PlainToken <: Token
+    content::String
+end
+struct InBracesToken <: Token
+    content::String
+end
+
+function transition(::Plain, str::String)
+    str == "{" && return SeenLbrace(), nothing
+    str == "}" && return SeenRbrace(), nothing
+    return Plain(), PlainToken(str)
+end
+function transition(::SeenLbrace, str::String)
+    str == "{" && return Plain(), PlainToken("{")
+    return InBraces(1, str), nothing
+end
+function transition(::SeenRbrace, str::String)
+    str == "}" && return Plain(), PlainToken("}")
+    throw(ErrorException("Unexpected } in f-string"))
+end
+function transition(state::InBraces, str::String)
+    level = state.nest_level
+    str == "{" && (level += 1)
+    str == "}" && (level -= 1)
+    level == 0 && return Plain(), InBracesToken(state.content)
+    return InBraces(level, state.content * str), nothing
+end
+
+
+make_expr(tok::PlainToken) = tok.content
+function make_expr(tok::InBracesToken)
+    last_colon_ix = findlast(':', tok.content)
+
+    parsed_before_colon = try
+        if last_colon_ix === nothing
+            nothing
+        else
+            p = Meta.parse(tok.content[begin:prevind(tok.content, last_colon_ix)])
+            p isa Expr && p.head == :incomplete ? nothing : p
+        end
+    catch e
+        if isa(e, Meta.ParseError) && occursin("colon expected", e.msg)
+            nothing
+        else
+            rethrow(e)
+        end
+    end
+    parsed, format_spec = if parsed_before_colon === nothing
+        Meta.parse(tok.content), "s"
+    else
+        parsed_before_colon, tok.content[nextind(tok.content, last_colon_ix):end]
+    end
+    expr = :(fmt($format_spec, $(esc(parsed))))
+    @debug "" tok expr
+    return expr
+end
+
+
 
 macro f_str(str)
-    @debug "" str
-    @debug tokenize(str) |> collect
-    toks = tokenize(str) |> collect
-    exprs = []
-    state = Dict(:name => :plain, :num_lbraces => 0, :num_rbraces => 0)
-    for (it, tok) in enumerate(toks)
-        @debug "" state tok
-        if Tokens.kind(tok) == Tokens.ERROR
-            throw(ErrorException("Error token in f-string: $tok"))
-        elseif Tokens.kind(tok) == Tokens.ENDMARKER
-            @assert it == length(toks)
-            break
-        end
-        tok_next = it < length(toks) ? toks[it + 1] : nothing
-        if state[:name] == :plain
-            if Tokens.kind(tok) == Tokens.LBRACE
-                if state[:num_lbraces] == 1
-                    push!(exprs, "{")
-                    state[:num_lbraces] = 0
-                    continue        
-                elseif tok_next != nothing && Tokens.kind(tok_next) == Tokens.LBRACE
-                    state[:num_lbraces] += 1
-                    continue
-                else
-                    state = Dict(:name => :in_braces, :parts => [], :level => 1)
-                    continue
-                end
-            elseif Tokens.kind(tok) == Tokens.RBRACE
-                if state[:num_rbraces] == 1
-                    push!(exprs, "}")
-                    state[:num_rbraces] = 0
-                    continue        
-                elseif tok_next != nothing && Tokens.kind(tok_next) == Tokens.RBRACE
-                    state[:num_rbraces] += 1
-                    continue
-                else
-                    throw(ErrorException("Unexpected } in f-string"))
-                end
-            end
-            push!(exprs, Tokens.untokenize(tok))
-        elseif state[:name] == :in_braces
-            if Tokens.kind(tok) == Tokens.LBRACE
-                state[:level] += 1
-            elseif Tokens.kind(tok) == Tokens.RBRACE
-                state[:level] -= 1
-                if state[:level] == 0
-                    expr = Meta.parse(join(state[:parts], ""))
-                    expr = :(fmt("s", $(esc(expr))))
-                    push!(exprs, expr)
-                    state = Dict(:name => :plain, :num_lbraces => 0, :num_rbraces => 0)
-                    continue
-                end
-            elseif state[:level] == 1 && Tokens.kind(tok) == Tokens.OP && Tokens.untokenize(tok) == ":"
-                try
-                    expr = Meta.parse(join(state[:parts], ""))
-                    if expr isa Expr && expr.head == :incomplete
-                        throw(Meta.ParseError("colon expected"))
-                    end
-                    state = Dict(:name => :format_spec, :value_expr => expr, :parts => [])
-                    continue
-                catch e
-                    if isa(e, Meta.ParseError) && occursin("colon expected", e.msg)
-                        # do nothing, current token will be added to parts below
-                    else
-                        rethrow(e)
-                    end
-                end
-            end
-            push!(state[:parts], Tokens.untokenize(tok))
-        elseif state[:name] == :format_spec
-            if Tokens.kind(tok) == Tokens.RBRACE
-                format_spec = join(state[:parts], "")
-                expr = :(fmt( $(esc(format_spec)), $(esc(state[:value_expr])) ))
-                push!(exprs, expr)
-                state = Dict(:name => :plain, :num_lbraces => 0, :num_rbraces => 0)
-                continue
-            end
-            push!(state[:parts], Tokens.untokenize(tok))
-        else
-            throw(ErrorException("Unsupported state: $state"))
-        end
+    raw_tokens = untokenize.(tokenize(str))
+    @debug "" str raw_tokens
+    state = Plain()
+    tokens = Token[]
+    for raw_tok in raw_tokens
+        @debug "" state raw_tok
+        state, tok = transition(state, raw_tok)
+        @debug "" tok
+        tok !== nothing && push!(tokens, tok)
     end
-    @debug "" state
-    if state != Dict(:name => :plain, :num_lbraces => 0, :num_rbraces => 0)
-        throw(ErrorException("Unterminated f-string: state $state"))
-    end
+    @debug "" tokens
+    state != Plain() && throw(ErrorException("Unterminated f-string: state $state"))
+    exprs = map(make_expr, tokens)
     @debug "" exprs
     expr = :(join([$(exprs...)], ""))
     @debug "" expr
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,30 +4,39 @@ using Logging
 
 # ConsoleLogger(stdout, Logging.Debug) |> global_logger
 
-a = 5
-б = 1.23456789
-@test f"abc {a} def" == "abc 5 def"
-@test f"{Vector{Any}()}" == "Any[]"
-@test f"{'{'}" == "{"
-@test f"""{"{a}"}""" == "{a}"
-@test f"""{f"{a}"}""" == "5"
-@test f"абв {б}" == "абв 1.23456789"
-@test f"abc {a:05d} def" == "abc 00005 def"
-@test f"абв {б:6.3f}" == "абв  1.235"
-@test f"а{{бв{{{{{{ }}{{{б:6.3f}" == "а{бв{{{ }{ 1.235"
-@test f"""{""}""" == ""
-@test f"""{"{}"}""" == "{}"
-@test f" {a * б:.2f} а{{бв{{{'{'}{{{{ }}{{{б:6.3f}" == " 6.17 а{бв{{{{ }{ 1.235"
-@test f"{ifelse(true, 1, 2)}" == "1"
-@test f"{true ? 1 : 2}" == "1"
-@test f"{true ? 1 : 2 :.2f}" == "1.00"
-@test f"{(:abc)}" == "abc"
-@test f"{(:abc):5s}" == "abc  "
+@testset begin
+    a = 5
+    б = 1.23456789
+    @test f"abc {a} def" == "abc 5 def"
+    @test f"{Vector{Any}()}" == "Any[]"
+    @test f"{'{'}" == "{"
+    @test f"""{"{a}"}""" == "{a}"
+    @test f"""{f"{a}"}""" == "5"
+    @test f"абв {б}" == "абв 1.23456789"
+    @test f"abc {a:05d} def" == "abc 00005 def"
+    @test f"абв {б:6.3f}" == "абв  1.235"
+    @test f"а{{бв{{{{{{ }}{{{б:6.3f}" == "а{бв{{{ }{ 1.235"
+    @test f"""{""}""" == ""
+    @test f"""{"{}"}""" == "{}"
+    @test f" {a * б:.2f} а{{бв{{{'{'}{{{{ }}{{{б:6.3f}" == " 6.17 а{бв{{{{ }{ 1.235"
+    @test f"{ifelse(true, 1, 2)}" == "1"
+    @test f"{true ? 1 : 2}" == "1"
+    @test f"{true ? 1 : 2 :.2f}" == "1.00"
+    @test f"{(:abc)}" == "abc"
+    @test f"{(:abc):5s}" == "abc  "
+    @test f"{a > 4 ? :abc : :def}" == "abc"
+    @test f"{:a == :a ? :abc : :def}" == "abc"
+    @test f"{NTuple{2}}{{{Val{Int64}}" == "Tuple{T,T} where T{Val{Int64}"
+    @test f"{{Vector{Int64}}}" == "{VectorInt64}"
+    @test f"{{Vector{Int64:7s}}}" == "{VectorInt64  }"
+    @test f"{push!(Vector{Symbol}(), a > 4 ? :abc : :def)}" == "[:abc]"
+    @test f"{:abc == :abc ? push!(Vector{Symbol}(), a > 4 ? :abc : :def) : nothing}" == "[:abc]"
 
-# workaround for all macro exceptions being wrapped in LoadError in Julia
-@test_throws ErrorException try @eval(f"{") catch err; throw(err.error) end
-@test_throws ErrorException try @eval(f"{'") catch err; throw(err.error) end
-@test_throws ErrorException try @eval(f""" {" """) catch err; throw(err.error) end
-@test_throws ErrorException try @eval(f"""{"}""") catch err; throw(err.error) end
-@test_throws ErrorException try @eval(f"{(}") catch err; throw(err.error) end
-@test_throws Exception try @eval(f"{(]}") catch err; throw(err.error) end
+    # workaround for all macro exceptions being wrapped in LoadError in Julia
+    @test_throws ErrorException try @eval(f"{") catch err; throw(err.error) end
+    @test_throws ErrorException try @eval(f"{'") catch err; throw(err.error) end
+    @test_throws ErrorException try @eval(f""" {" """) catch err; throw(err.error) end
+    @test_throws ErrorException try @eval(f"""{"}""") catch err; throw(err.error) end
+    @test_throws ErrorException try @eval(f"{(}") catch err; throw(err.error) end
+    @test_throws Exception try @eval(f"{(]}") catch err; throw(err.error) end
+end
